521465a5cd0a0938924f6beac69050056e871488
vwheatle <vwheatle@kent.edu>
2022-04-10 02:04:15 -0400

    pretty colors

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -eu
 
+# Writes a colorful indented message. Very cool!
+function sayThing {
+	printf "\t\033[32m%s\033[0m\n" "$1"
+}
+
 # Yes this is slightly redundant, but also it works!
 
 if [[ "$@" =~ "clean" ]]
 then
-	echo "Cleaning..."
+	sayThing "Cleaning..."
 	rm -rf ./out
 	mkdir -p out
 	exit 0
@@ -15,19 +20,19 @@ mkdir -p out
 
 if [[ "$@" =~ "debug" ]] || [[ "$@" =~ "valgrind" ]]
 then
-	echo "Building debug version..."
+	sayThing "Building debug version..."
 	g++ -g src/main.cpp -Wall -Wunused -Wuninitialized --std=c++17 -o out/banker
 else
-	echo "Building..."
+	sayThing "Building..."
 	g++ src/main.cpp -Wall -Wunused -Wuninitialized --std=c++17 -o out/banker
 fi
 
 if [[ "$@" =~ "valgrind" ]]
 then
-	echo "Running Valgrind..."
-	(cd out && valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=2 --track-fds=yes ./banker)
+	sayThing "Running Valgrind with example data..."
+	(cd out && valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=2 --track-fds=yes ./banker ../src/data.txt)
 elif [[ "$@" =~ "run" ]]
 then
-	echo "Running..."
-	(cd out && ./banker)
+	sayThing "Running with example data..."
+	(cd out && ./banker ../src/data.txt)
 fi
