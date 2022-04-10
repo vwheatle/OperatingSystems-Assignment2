998208cd508e6af457af141fdd0d05079a4ed92b
vwheatle <vwheatle@kent.edu>
2022-04-09 20:07:09 -0400

    better build script

--- build.sh
+++ build.sh
@@ -3,5 +3,23 @@ set -eu
 
 mkdir -p out
 
-echo "Building debug version..."
-g++ -g src/main.cpp -Wall -Wunused -Wuninitialized --std=c++17 -o out/banker
+# Yes this is slightly redundant, but also it works!
+
+if [[ "$@" =~ "debug" ]] || [[ "$@" =~ "valgrind" ]]
+then
+	echo "Building debug version..."
+	g++ -g src/main.cpp -Wall -Wunused -Wuninitialized --std=c++17 -o out/banker
+else
+	echo "Building..."
+	g++ src/main.cpp -Wall -Wunused -Wuninitialized --std=c++17 -o out/banker
+fi
+
+if [[ "$@" =~ "valgrind" ]]
+then
+	echo "Running Valgrind..."
+	(cd out && valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=2 --track-fds=yes ./banker)
+elif [[ "$@" =~ "run" ]]
+then
+	echo "Running..."
+	(cd out && ./banker)
+fi
